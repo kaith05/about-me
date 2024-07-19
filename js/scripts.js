6be65ddc88fef6e4808c3686c906d304ae4f7c5a
kaito <168144956+kaith05@users.noreply.github.com>
2024-07-19 12:11:23 +0900

    update swiper

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,27 @@
-/*ハンバーガメニュー */
-const hamburgerBtn = document.getElementById('hamburger-btn');
-    const navbarNav = document.getElementById('navbar-nav');
-
-    hamburgerBtn.addEventListener('click', () => {
-    navbarNav.classList.toggle('show');
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 0, // スライド間の隙間をなくす
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // 800px以上の場合
+            800: {
+                slidesPerView: 1.5,
+                spaceBetween: 0, // スライド間の隙間をなくす
+            }
+        }
     });
-
-
-/*スライダー*/
-const swiper = new Swiper('.swiper', {
-    // ナビゲーション矢印
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    // 自動再生
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    // その他の設定
-    speed: 300,
-    loop: true,
-    loopAdditionalSlides: 1,
-    // ページネーション（必要な場合）
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    // スクロールバー（必要な場合）
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-    slidesPerView: 3,
-    spaceBetween: 30,
 });
